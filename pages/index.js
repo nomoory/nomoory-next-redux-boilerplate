8c6f7b3b043493d8f9776d03a12ced3757112857
junhyek <junhyek@noom.com>
2019-11-27 18:04:49 +0900

    Bring text to center

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
+const centerStyle = {
+    display: 'flex',
+    width: '100vw',
+    height: '100vh',
+    alignItems: 'center',
+    justifyContent: 'center',
+};
+
 function Home() {
-    return <div>Welcome to Next.js!</div>
+    return <div style={centerStyle}>Welcome to Next.js!</div>
 }
 
 export default Home
